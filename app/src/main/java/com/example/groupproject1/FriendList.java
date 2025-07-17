10c3020772a5abc6b154722dc87edd293569b8b0
tqors <133776852+tqors@users.noreply.github.com>
2025-07-17 21:05:42 +0800

    test :p

---
+++ app/src/main/java/com/example/groupproject1/FriendList.java
@@ -0,0 +1,26 @@
+package com.example.groupproject1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FriendList extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_list);
+
+        recyclerView = findViewById(R.id.main_recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
